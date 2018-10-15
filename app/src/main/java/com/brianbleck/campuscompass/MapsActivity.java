e459b0115af599653c4f0ea77fb6a18944dc540d
brianb <bbleck@unm.edu>
2018-10-15 10:30:01 -0600

    Added options menu

--- app/src/main/java/com/brianbleck/campuscompass/MapsActivity.java
+++ app/src/main/java/com/brianbleck/campuscompass/MapsActivity.java
@@ -3,6 +3,11 @@ package com.brianbleck.campuscompass;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,7 +15,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
   private GoogleMap mMap;
 
@@ -18,10 +23,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_maps);
+    initToolBar();
     // Obtain the SupportMapFragment and get notified when the map is ready to be used.
     SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
         .findFragmentById(R.id.map);
     mapFragment.getMapAsync(this);
+  }
+
+  private void initToolBar() {
+//    Toolbar toolbar = findViewById(R.id.toolbar);
+//    setSupportActionBar(toolbar);
   }
 
 
@@ -41,5 +52,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     LatLng sydney = new LatLng(-34, 151);
     mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
     mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.item_menu, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()){
+      case R.id.blue_phone:
+        return true;
+      case R.id.building:
+        return true;
+      case R.id.restroom:
+        return true;
+      case R.id.healthy_vending:
+        return true;
+      case R.id.dining:
+        return true;
+      case R.id.computer_pod:
+        return true;
+      case R.id.breast_feeding:
+        return true;
+        default:
+          return super.onOptionsItemSelected(item);
+    }
   }
 }
