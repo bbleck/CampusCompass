66522923e36b0a740dcd11799682451ce8eda562
bBleck <bmbleck@hotmail.com>
2018-10-25 11:54:15 -0600

    Added custom item for SearchFrag recyclerview.

--- app/src/main/java/com/brianbleck/campuscompass/view/MainMenuFragment.java
+++ app/src/main/java/com/brianbleck/campuscompass/view/MainMenuFragment.java
@@ -4,18 +4,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import com.brianbleck.campuscompass.R;
 import com.brianbleck.campuscompass.controller.MainActivity;
-
-import java.util.Objects;
 
 public class MainMenuFragment extends Fragment {
 
