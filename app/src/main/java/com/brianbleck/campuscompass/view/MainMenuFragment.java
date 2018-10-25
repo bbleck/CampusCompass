3322069f4ace78a4d279cac2e7f99a5173f7b544
bBleck <bmbleck@hotmail.com>
2018-10-25 08:56:47 -0600

    Fixed bug that was crashing on looping through setting viewIds in MainMenuFragment.

--- app/src/main/java/com/brianbleck/campuscompass/view/MainMenuFragment.java
+++ app/src/main/java/com/brianbleck/campuscompass/view/MainMenuFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +19,7 @@ import java.util.Objects;
 
 public class MainMenuFragment extends Fragment {
 
+    private static final String TAG = "MainMenuFragment";
 
     public static final int COUNT_SEARCHES = 9;
     private ImageView[] imageItems = new ImageView[COUNT_SEARCHES];
@@ -35,18 +37,21 @@ public class MainMenuFragment extends Fragment {
 
     private void initViews(View theView) {
         for (int i = 0; i < COUNT_SEARCHES ; i++) {
-            final int id = Objects.requireNonNull(getContext()).getResources().getIdentifier("iv_main_frag_" + i, "id", theView.getContext().getPackageName());
-            int idTxt = Objects.requireNonNull(getContext()).getResources().getIdentifier("tv_main_frag_" + i, "id", theView.getContext().getPackageName());
-            final int idTxtSetTxt = Objects.requireNonNull(getContext()).getResources().getIdentifier("image_title_" + i, "id", theView.getContext().getPackageName());
+            final int id = getResources().getIdentifier("iv_main_frag_" + i, "id", getContext().getPackageName());
+            String temp1 = Integer.toString(id);
+            Log.d(TAG, "initViews: id is " + temp1);
+            int idTxt = getResources().getIdentifier("tv_main_frag_" + i, "id", getContext().getPackageName());
+            Log.d(TAG, "initViews: " + idTxt + " " + getString(idTxt));
+            final String idTxtSetTxt = getString(getResources().getIdentifier("image_title_" + i, "string", getContext().getPackageName()));
+            Log.d(TAG, "initViews: string will be " + idTxtSetTxt);
             imageItems[i] = theView.findViewById(id);
             textItems[i] = theView.findViewById(idTxt);
             textItems[i].setText(idTxtSetTxt);
             imageItems[i].setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Toast.makeText(getActivity(), "Clicked on item: " + getString(idTxtSetTxt), Toast.LENGTH_SHORT).show();
-                    //todo: remove toast and add logic to navigate to new fragment
-                    ((MainActivity)getActivity()).setmViewPager(MainActivity.SEARCH_FRAG_PAGER_NUMBER, id);
+                    Toast.makeText(getActivity(), "Clicked on item: " + idTxtSetTxt, Toast.LENGTH_SHORT).show();
+//                    ((MainActivity)getActivity()).setmViewPager(MainActivity.SEARCH_FRAG_PAGER_NUMBER, id);
                 }
             });
         }
