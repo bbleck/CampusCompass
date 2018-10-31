451cc820aa221def48406a41f458a5d31a080877
bBleck <bmbleck@hotmail.com>
2018-10-31 13:51:18 -0600

    Added RxJava dependencies.

---
+++ app/src/main/java/com/brianbleck/campuscompass/controller/UnmDataService.java
@@ -0,0 +1,12 @@
+package com.brianbleck.campuscompass.controller;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface UnmDataService {
+
+//    @GET("diceware")
+//    Call<String[]> get(@Query("length") int length);
+//
+//  }
+}
