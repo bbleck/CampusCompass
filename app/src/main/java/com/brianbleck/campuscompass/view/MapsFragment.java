1179455363d47d8bac259d113cd41840337b8939
bBleck <bmbleck@hotmail.com>
2018-11-05 14:36:33 -0700

    Cleaned the dead code out of MapsFragment.

--- app/src/main/java/com/brianbleck/campuscompass/view/MapsFragment.java
+++ app/src/main/java/com/brianbleck/campuscompass/view/MapsFragment.java
@@ -5,33 +5,28 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.brianbleck.campuscompass.R;
-import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 
 public class MapsFragment extends Fragment {
 
     private static final String TAG = "MapsFragment";
-    private MapsFragmentListener mapsfragListener;
-    private FragmentActivity mActivity;
+    private MapsFragmentListener mapsFragmentListener;
 
     public interface MapsFragmentListener{
         void setMainRefMapsFrag(MapsFragment mapsFrag);
         void callMapAsync(SupportMapFragment supportMapFragment);
     }
 
-    private GoogleMap googleMap;
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View theView = inflater.inflate(R.layout.fragment_maps, container, false);
-        mapsfragListener.setMainRefMapsFrag(this);
+        mapsFragmentListener.setMainRefMapsFrag(this);
         initMaps();
         return theView;
     }
@@ -44,36 +39,17 @@ public class MapsFragment extends Fragment {
         getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frag_container_2, mapFragment)
             .commit();
       }
-//        mapFragment.getMapAsync(this);
-        mapsfragListener.callMapAsync(mapFragment);
-//      Toast.makeText(getActivity(), "getmapasync called.", Toast.LENGTH_SHORT).show();
+        mapsFragmentListener.callMapAsync(mapFragment);
     }
-
-//    @Override
-//    public void onMapReady(GoogleMap gMap) {
-//      Toast.makeText(getActivity(), "into onMapReady", Toast.LENGTH_SHORT).show();
-//        this.googleMap = gMap;
-//        // Add a marker in Sydney and move the camera
-//        LatLng sydney = new LatLng(-34, 151);
-//        this.googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-//        this.googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-//    }
-
+    
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
         try{
-            mapsfragListener = (MapsFragmentListener) getActivity();
-            mActivity = getActivity();
+            mapsFragmentListener = (MapsFragmentListener) getActivity();
         } catch (ClassCastException e){
             Log.e(TAG, "onAttach: ClassCastException" + e.getMessage());
         }
 
     }
-
-  @Override
-  public void onDestroyView() {
-
-    super.onDestroyView();
-  }
 }
